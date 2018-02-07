ecf160125d7c8e8d07afca3c4e2dbb42bd2aed5a
maloso567 <32587217+maloso567@users.noreply.github.com>
2018-02-07 17:53:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/malos/siie/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.malos.siie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void ingresar(View view){
+        Intent v3= new Intent(MainActivity.this,Main2Activity.class);
+
+        startActivity(v3);
+        finish();
+
+    }
+}
